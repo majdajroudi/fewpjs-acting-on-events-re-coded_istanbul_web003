146720c2c7d27e4fc0117b0887b9d3398df2bcee
Majd Ajroudi <majd.ajroudi@gmail.com>
2020-10-31 22:50:36 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+let dodger = document.getElementById("dodger")
+dodger.style.backgroundColor = "#FF69B4";
+console.log(dodger.style.left); // "180px"
+console.log(dodger.style.bottom); // "0px"
+dodger.style.bottom = "0px";
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+   
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+function moveDodgerRight() {
+let rightNumbers = dodger.style.left.replace("px", "");
+let left = parseInt(rightNumbers, 10);
+
+if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+}}
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+    else if (e.key === "ArrowRight") {
+        moveDodgerRight();
+        console.log(dodger.style.left)
+    }
+  });
